fd9b68a6630a33e448952e275340136d088d7527
maxx.diego <77diego.max@gmail.com>
2024-07-13 17:00:30 -0300

    @maxxdiego

--- tailwind_11-projeto-02-facebook-clone/tailwind.config.js
+++ tailwind_11-projeto-02-facebook-clone/tailwind.config.js
@@ -4,28 +4,21 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        '14': '3.5rem',
-        '125px': '125px'
-      },
-      colors: {
-        'hoverspt': '#18D760',
-        'purple-main': '#2D46B9',
-        'green-main': '#1ED760'
-      },
-      backgroundImage: theme => ({
-        'spotify-theme': "url('/img/bursts.svg')",
-        'spotify-theme-mobile': "url('/img/bursts-mobile.svg')"
-      }),
-      backgroundSize: {
-        '175%': '175%',
-        '195%': '195%'
-      },
-      backgroundPosition: {
-        'banner': '46% 4%',
-        'banner-mobile': 'top 25% center'
+        '30': '7.5rem',
+        '320px': '320px',
+        '396px': '396px',
+        '500px': '500px',
+        '980px': '980px'
       },
       fontSize: {
-        '9xl': '9rem'
+        'subtitle': '1.7rem'
+      },
+      colors: {
+        'main-gray': '#f0f2f5',
+        'main-blue': '#0866FF',
+        'main-blue-hover': '#1877F2',
+        'main-green': '#42b72a',
+        'main-green-hover': '#36a420'
       }
     },
   },
